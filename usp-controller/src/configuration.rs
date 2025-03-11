970e6f8fa5e6277cbca4a8a1fecd2d5178ae5c1e
TueTran2008 <tuetd174343@gmail.com>
2025-03-11 23:03:28 +0700

    add config

---
+++ usp-controller/src/configuration.rs
@@ -0,0 +1,12 @@
+use config::Config;
+struct DatabaseSetting {
+    pub username: String,
+    pub password: String,
+    pub port: u16,
+    pub host: String,
+    pub database_name: String,
+}
+
+impl DatabaseSetting {
+    //mongodb:.//user:pass@sample.host:port/?Optio
+}
--- usp-controller/src/lib.rs
+++ usp-controller/src/lib.rs
@@ -1,3 +1,4 @@
+pub mod configuration;
 pub mod mqtt_client;
 pub mod protobuf;
 pub mod telemetry;
